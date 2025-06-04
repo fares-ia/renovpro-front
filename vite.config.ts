506a140e4b1238ecff29f39aac4752f653fcd3cc
fares <faresbenlakehal@gmail.com>
2025-06-04 19:06:12 +0200

    probleme page admin access fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,23 @@ export default defineConfig(({ mode }) => ({
       output: {
         manualChunks: undefined,
       },
+      external: [
+        '@radix-ui/react-checkbox',
+        '@radix-ui/react-alert-dialog',
+        '@radix-ui/react-avatar',
+        '@radix-ui/react-dialog',
+        '@radix-ui/react-dropdown-menu',
+        '@radix-ui/react-label',
+        '@radix-ui/react-navigation-menu',
+        '@radix-ui/react-popover',
+        '@radix-ui/react-radio-group',
+        '@radix-ui/react-select',
+        '@radix-ui/react-separator',
+        '@radix-ui/react-slot',
+        '@radix-ui/react-tabs',
+        '@radix-ui/react-toast',
+        '@radix-ui/react-tooltip'
+      ]
     },
   },
   base: '/',
